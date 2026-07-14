33c2453373d5e38164fbe4ebe9efb7d943ba18ef
Enzo Basuil <jlcbasuil@gmail.com>
2026-07-14 11:24:40 +0800

    Created initial project setup for Capstone Project
    Created initial project setup for Capstone Project

---
+++ app/src/main/java/com/example/cpe3323_capstone_cookbook/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.cpe3323_capstone_cookbook
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cpe3323_capstone_cookbook.ui.theme.CpE3323_Capstone_CookbookTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CpE3323_Capstone_CookbookTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CpE3323_Capstone_CookbookTheme {
+        Greeting("Android")
+    }
+}
